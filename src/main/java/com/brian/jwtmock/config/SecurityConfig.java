bc013feab1ac9dd0f33077e00738630e1c29d547
Braian Rojas <braianrojas95@gmail.com>
2025-05-03 19:01:41 -0300

    Implementación de login mockeado con validación por Postman

---
+++ src/main/java/com/brian/jwtmock/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.brian.jwtmock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable()); // Desactiva el login clásico
+
+        return http.build();
+    }
+}
